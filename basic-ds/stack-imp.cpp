d9cb30a526028970b40d8c38ab67c04811403315
txrunishere <tsoni9286@gmail.com>
2026-05-09 13:53:27 +0530

    implement stack using vector

--- basic-ds/stack-imp.cpp
+++ basic-ds/stack-imp.cpp
@@ -2,11 +2,51 @@
 #include <vector>
 using namespace std;
 
+// stack using vector
 class Stack
 {
+  vector<int> v;
+
+public:
+  int get_top()
+  {
+    return v[v.size() - 1];
+  }
+
+  void push(int val)
+  {
+    v.push_back(val);
+  }
+
+  void pop()
+  {
+    v.pop_back();
+  }
+
+  bool empty()
+  {
+    return v.size() == 0;
+  }
+
+  void print()
+  {
+    while (!this->empty())
+    {
+      cout << this->get_top() << " ";
+      this->pop();
+    }
+    cout << endl;
+  }
 };
 
 int main()
 {
+  Stack s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  cout << s.get_top() << endl;
+  s.print();
+
   return 0;
 }
