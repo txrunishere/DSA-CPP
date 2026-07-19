29ea66417df845617533c507056af449351fe7c4
Tarun <tsoni9286@gmail.com>
2026-07-19 21:45:52 +0530

    selection sort

--- searching-sorting/sorting/selection-sort.cpp
+++ searching-sorting/sorting/selection-sort.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 using namespace std;
 
+void selection_sort(int arr[], int n)
+{
+    if (n == 0)
+    {
+        cout << "Array is empty" << endl;
+        return;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        swap(arr[min], arr[i]);
+    }
+}
+
 int main()
 {
+    int n;
+    cout << "Define the length of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter values for array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    selection_sort(arr, n);
+
+    cout << endl;
+    cout << "Sorted Array" << endl;
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
     return 0;
 }
