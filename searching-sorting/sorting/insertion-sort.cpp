5938e9f67b7476e6d287eec4e0685543615e6e78
Tarun <tsoni9286@gmail.com>
2026-07-19 22:03:40 +0530

    insertion sort

--- searching-sorting/sorting/insertion-sort.cpp
+++ searching-sorting/sorting/insertion-sort.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void insertion_sort(int arr[], int n)
+{
+    if (n == 0)
+    {
+        cout << "Array is empty" << endl;
+        return;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int j = i;
+
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+}
+
 int main()
 {
+    int n;
+    cout << "Define the length of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter values for array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr, n);
+
+    cout << endl;
+    cout << "Sorted Array" << endl;
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
     return 0;
 }
